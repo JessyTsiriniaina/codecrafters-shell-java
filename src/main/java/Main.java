3fb3722537cbcc08a997f0cecd3154c8d1b344b0
Jessy <tsiriniainajessy2@gmail.com>
2025-03-26 02:46:54 +0300

    uncomment correction curly bracet

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception 
+    public static void main(String[] args) throws Exception {
         System.out.print("$ ");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
