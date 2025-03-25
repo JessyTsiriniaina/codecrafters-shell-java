0ceff8dbf7cd85277f8539c2a1ec367a16def937
Jessy <tsiriniainajessy2@gmail.com>
2025-03-26 02:46:08 +0300

    uncomment correction

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception 
         System.out.print("$ ");
-
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
     }
