abed378dc29f9db31940806116555721f1e056b8
Jessy Tsiriniaina <tsiriniainajessy2@gmail.com>
2025-03-26 09:42:59 +0300

    echo functionnality

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -6,6 +8,18 @@ public class Main {
             System.out.print("$ ");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
+            Pattern pattern = Pattern.compile("^echo");
+            Matcher matcher = pattern.matcher(input);
+
+            if(matcher.find()) {
+                String[] splittedInput = input.split(" ");
+                System.out.println();
+                for(int i = 1; i < splittedInput.length; i++) {
+                    System.out.print(splittedInput[i] + " ");
+                }
+                break;
+            }
+            
             if(input.equals("exit 0")) {
                 break;
             }
