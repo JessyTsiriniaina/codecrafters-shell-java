c375d5726469f14a73c79e3bb0770180af4a535c
Jessy Tsiriniaina <tsiriniainajessy2@gmail.com>
2025-03-27 20:34:23 +0300

    type builtin executable files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 import java.util.Arrays;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Main {
 
@@ -10,8 +12,21 @@ public class Main {
     public static void type(String command, String[] builtinCommands) {
         if(Arrays.asList(builtinCommands).contains(command)) 
             System.out.println(command + " is a shell builtin");
+        else if(getPath(command) != null) {
+            System.out.println(command + " is " + getPath(command));
+        }
         else System.out.println(command + ": not found");
     }
+
+    public static String getPath(String parameter) {
+        for (String path : System.getenv("PATH").split(":")) {
+          Path fullPath = Path.of(path, parameter);
+          if (Files.isRegularFile(fullPath)) {
+            return fullPath.toString();
+          }
+        }
+        return null;
+      }
 
     public static void main(String[] args) throws Exception {
         while (true) {
@@ -22,22 +37,19 @@ public class Main {
             String[] splittedInput = input.split(" ");
             String command = splittedInput[0];
 
+            if (input.equals("exit 0")) {
+                System.exit(0);
+            }
             
-            if (Arrays.asList(builtinCommands).contains(command)) {
-                if (input.equals("exit 0")) {
+            switch (command) {
+                case "echo":
+                    echo(input);
                     break;
-                }
-                switch (command) {
-                    case "echo":
-                        echo(input);
-                        break;
-                    case "type": 
+                case "type": 
+                    if(splittedInput.length > 1)
                         type(splittedInput[1], builtinCommands);
-                        break;
-                }
-                
-            } else {
-                System.out.println(input + ": command not found");
+                    break;
+                default: System.out.println(input + ": command not found");
             }
         }
     }
