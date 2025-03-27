3b988696ae5ab69bd33341d30d8f08c008a8ebf8
Jessy Tsiriniaina <tsiriniainajessy2@gmail.com>
2025-03-27 18:51:16 +0300

    added type builtin functionnality

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,49 @@
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.*;
+// import java.util.regex.Matcher;
+// import java.util.regex.Pattern;
 
 public class Main {
+
+    public static void echo(String[] splittedInput) {
+        for (int i = 1; i < splittedInput.length; i++) {
+            System.out.print(splittedInput[i]);
+            System.out.print(i != (splittedInput.length - 1) ? " " : "");
+        }
+        System.out.println();
+    }
+
+    public static void type(String[] splittedInput, String[] builtinCommands) {
+        if(Arrays.asList(builtinCommands).contains(splittedInput[1])) {
+            System.out.println(splittedInput[1] + " is a shell builtin");
+        }
+        else System.out.println(splittedInput[1] + ": not found");
+    }
     public static void main(String[] args) throws Exception {
         while (true) {
             System.out.print("$ ");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
-            Pattern pattern = Pattern.compile("^echo");
-            Matcher matcher = pattern.matcher(input);
+            // Pattern pattern = Pattern.compile("^echo");
+            // Matcher matcher = pattern.matcher(input);
+            String[] builtinCommands = {"echo", "type", "exit"};
+            String[] splittedInput = input.split(" ");
+            String command = splittedInput[0];
 
             
-            if(input.equals("exit 0")) {
-                break;
-            }
-            if(matcher.find()) {
-                String[] splittedInput = input.split(" ");
-                for(int i = 1; i < splittedInput.length; i++) {
-                    System.out.print(splittedInput[i]);
-                    System.out.print(i != (splittedInput.length - 1) ? " " : "");
+            if (Arrays.asList(builtinCommands).contains(command)) {
+                if (command.equals("exit")) {
+                    break;
                 }
-                System.out.println();
-            } 
-            else {
+                switch (command) {
+                    case "echo":
+                        echo(splittedInput);
+                        break;
+                    case "type": 
+                        type(splittedInput, builtinCommands);
+                        break;
+                }
+                
+            } else {
                 System.out.println(input + ": command not found");
             }
         }
