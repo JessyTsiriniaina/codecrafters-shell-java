490aaaf5224a197169fa2843d69e1790d4ffb02f
Jessy Tsiriniaina <tsiriniainajessy2@gmail.com>
2025-03-27 19:09:40 +0300

    refactor type builtin functionnality

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,45 +1,38 @@
-import java.util.*;
-// import java.util.regex.Matcher;
-// import java.util.regex.Pattern;
+import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
 
-    public static void echo(String[] splittedInput) {
-        for (int i = 1; i < splittedInput.length; i++) {
-            System.out.print(splittedInput[i]);
-            System.out.print(i != (splittedInput.length - 1) ? " " : "");
-        }
-        System.out.println();
+    public static void echo(String input) {
+        System.out.println(input.substring(5));
     }
 
-    public static void type(String[] splittedInput, String[] builtinCommands) {
-        if(Arrays.asList(builtinCommands).contains(splittedInput[1])) {
-            System.out.println(splittedInput[1] + " is a shell builtin");
-        }
-        else System.out.println(splittedInput[1] + ": not found");
+    public static void type(String command, String[] builtinCommands) {
+        if(Arrays.asList(builtinCommands).contains(command)) 
+            System.out.println(command + " is a shell builtin");
+        else System.out.println(command + ": not found");
     }
+
     public static void main(String[] args) throws Exception {
         while (true) {
             System.out.print("$ ");
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
-            // Pattern pattern = Pattern.compile("^echo");
-            // Matcher matcher = pattern.matcher(input);
             String[] builtinCommands = {"echo", "type", "exit"};
             String[] splittedInput = input.split(" ");
             String command = splittedInput[0];
 
             
             if (Arrays.asList(builtinCommands).contains(command)) {
-                if (command.equals("exit")) {
+                if (command.equals("exit 0")) {
                     break;
                 }
                 switch (command) {
                     case "echo":
-                        echo(splittedInput);
+                        echo(input);
                         break;
                     case "type": 
-                        type(splittedInput, builtinCommands);
+                        type(splittedInput[1], builtinCommands);
                         break;
                 }
                 
